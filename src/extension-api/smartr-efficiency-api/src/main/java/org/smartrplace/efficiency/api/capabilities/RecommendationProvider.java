9445baddcdf8885d8139c6aa1bdbc22f1efe8545
dnestle <dnestle@192.168.64.176>
2018-02-16 17:17:38 +0100

    restructured API draft and example

---
+++ src/extension-api/smartr-efficiency-api/src/main/java/org/smartrplace/efficiency/api/capabilities/RecommendationProvider.java
@@ -0,0 +1,42 @@
+package org.smartrplace.efficiency.api.capabilities;
+
+import java.util.List;
+
+import org.smartrplace.efficiency.api.base.SmartEffExtensionResourceType;
+import org.smartrplace.extensionservice.ExtensionCapability;
+
+import de.iwes.timeseries.eval.api.LabelledItem;
+import extensionmodel.smarteff.api.base.SmartEffGeneralData;
+import extensionmodel.smarteff.api.base.SmartEffUserData;
+
+/** To be provided as OSGi service
+  * */
+public interface RecommendationProvider extends ExtensionCapability {
+	/** TODO: Should we rather provide a Recommendation resource here? This makes the interface much more flexible,
+	if an older version of a data model is used just some fields may be missing, would be replaced by default value.
+	With this solution the main domain app will have to catch NoSuchMethods-Exceptions and fill in defaults.
+	*/
+	public interface Recommendation extends LabelledItem {
+		float getNetInvestment();
+		float getAnnualNetSavings();
+		float getAnnualCO2Savings();
+		//...
+	}
+	
+	/** If any resource of a type listed here changes the updateRecommendations method shall be
+	 * called. Note that not listeners on all elements may be registered, but the application may
+	 * only trigger this if it changed a value itself or let an extension make changes
+	 */
+	public List<Class<? extends SmartEffExtensionResourceType>> inputResourceTypes();
+	
+	/** Trigger initial calculation of recommendations or update for a certain user
+	 * 
+	 * @param userData reference to data of the user
+	 * @param generalData
+	 * @param resourcesChanged known resources to be changed. If null the recommendations shall be re-calculated entirely
+	 * @param recommendations may be an emtpy list on initial calculation
+	 */
+	public void updateRecommendations(SmartEffUserData userData, SmartEffGeneralData generalData,
+			List<SmartEffExtensionResourceType> resourcesChanged,
+			List<Recommendation> recommendations);
+}
