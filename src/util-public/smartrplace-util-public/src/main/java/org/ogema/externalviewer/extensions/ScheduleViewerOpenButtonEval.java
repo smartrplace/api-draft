dd45d6e6242afeee547bc62b70ce4518ad995012
dnestle <dnestle@10.13.91.75>
2019-08-06 19:46:42 +0200

    source code as of smartrplace-util-proposed version 0.9.0.3

--- src/util-public/smartrplace-util-public/src/main/java/org/ogema/externalviewer/extensions/ScheduleViewerOpenButtonEval.java
+++ src/util-public/smartrplace-util-public/src/main/java/org/ogema/externalviewer/extensions/ScheduleViewerOpenButtonEval.java
@@ -41,6 +41,7 @@ import de.iwes.widgets.api.widgets.sessionmanagement.OgemaHttpRequest;
 import de.iwes.widgets.reswidget.scheduleviewer.api.ScheduleViewerConfiguration;
 import de.iwes.widgets.reswidget.scheduleviewer.api.ScheduleViewerConfigurationBuilder;
 import de.iwes.widgets.reswidget.scheduleviewer.api.TimeSeriesFilter;
+import de.iwes.widgets.reswidget.scheduleviewer.api.TimeSeriesFilterExtended;
 
 public abstract class ScheduleViewerOpenButtonEval extends ScheduleViewerOpenButton {
 	private static final long serialVersionUID = 1L;
@@ -60,15 +61,17 @@ public abstract class ScheduleViewerOpenButtonEval extends ScheduleViewerOpenBut
 			scheduleViewerProviderInstance);
 	}
 	
-	@SuppressWarnings("unchecked")
 	@Override
 	public void onPrePOST(String data, OgemaHttpRequest req) {
 		//final GaRoSingleEvalProvider eval = selectProvider.getSelectedItem(req);
 		
 		List<TimeSeriesData> input = getTimeseries(req);
-		
-		ReadOnlyTimeSeries timeSeries;
-		List<ReadOnlyTimeSeries> result = new ArrayList<>();
+		TimeSeriesWithFilters filteringResult = getTimeSeriesWithFilters(input, "Filter for "+getEvaluationProviderId(req));
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		List<TimeSeriesFilter> programsInner = (List)filteringResult.filters;
+		//List<ReadOnlyTimeSeries> result = new ArrayList<>();
+
+		/*ReadOnlyTimeSeries timeSeries;
 		Map<String, String> shortNamesRD = new HashMap<String, String>();
 		Map<String, String> longNamesRD = new HashMap<String, String>();
 		Map<ReadOnlyTimeSeries, String> shortNames = new HashMap<ReadOnlyTimeSeries, String>();
@@ -137,10 +140,10 @@ public abstract class ScheduleViewerOpenButtonEval extends ScheduleViewerOpenBut
 			}
 			if(timeSeries != null) result.add(timeSeries);
 		}
-		List<Collection<TimeSeriesFilter>> programs = new ArrayList<>();
 		List<TimeSeriesFilter> programsInner = new ArrayList<>();
 		programsInner.add(new DefaultTimeSeriesFilterExtended("Filter for "+getEvaluationProviderId(req), shortNames, longNames,
-				shortNamesRD, longNamesRD, types, typesRD));
+				shortNamesRD, longNamesRD, types, typesRD));*/
+		List<Collection<TimeSeriesFilter>> programs = new ArrayList<>();
 		programs.add(programsInner);
 		
 		//String config = selectConfig.getSelectedLabel(req);
@@ -159,7 +162,7 @@ public abstract class ScheduleViewerOpenButtonEval extends ScheduleViewerOpenBut
 				ScheduleViewerConfigurationBuilder.newBuilder().setPrograms(programs).
 				setStartTime(startTime).setEndTime(endTime).setShowManipulator(true).build();
 		
-		String ci = addConfig(new DefaultDedicatedTSSessionConfiguration(result, viewerConfiguration));
+		String ci = addConfig(new DefaultDedicatedTSSessionConfiguration(filteringResult.timeSeries, viewerConfiguration));
 		setConfigId(ci, req);
 	}
 	
@@ -174,4 +177,87 @@ public abstract class ScheduleViewerOpenButtonEval extends ScheduleViewerOpenBut
 		return parts[2].substring(parts[2].length()-4);	
 	}
 
+	public static class TimeSeriesWithFilters {
+		public List<ReadOnlyTimeSeries> timeSeries = new ArrayList<>();
+		public List<TimeSeriesFilterExtended> filters;
+	}
+	@SuppressWarnings("unchecked")
+	public static TimeSeriesWithFilters getTimeSeriesWithFilters(List<TimeSeriesData> input, String filterName) {
+		ReadOnlyTimeSeries timeSeries;
+		//List<ReadOnlyTimeSeries> result = new ArrayList<>();
+		TimeSeriesWithFilters result = new TimeSeriesWithFilters();
+		Map<String, String> shortNamesRD = new HashMap<String, String>();
+		Map<String, String> longNamesRD = new HashMap<String, String>();
+		Map<ReadOnlyTimeSeries, String> shortNames = new HashMap<ReadOnlyTimeSeries, String>();
+		Map<ReadOnlyTimeSeries, String> longNames = new HashMap<ReadOnlyTimeSeries, String>();
+		Map<ReadOnlyTimeSeries, Class<?>> types = new HashMap<>();
+		Map<String, Class<?>> typesRD = new HashMap<>();
+		for (TimeSeriesData tsdBase : input) {
+			if(!(tsdBase instanceof TimeSeriesDataOffline)) throw new IllegalStateException("getStartAndEndTime only works on TimeSeriesData input!");
+			TimeSeriesDataOffline tsd = (TimeSeriesDataOffline) tsdBase;
+			timeSeries = tsd.getTimeSeries();
+			String tsId;
+			String shortName;
+			String longName;
+			if(timeSeries instanceof RecordedData) {
+				tsId = ((RecordedData)timeSeries).getPath();
+			} else tsId = null;
+			GaRoDataTypeI dataType = null;
+			Class<? extends Resource> type = null;
+			if(tsd instanceof TimeSeriesDataExtendedImpl) {
+				TimeSeriesDataExtendedImpl tse = (TimeSeriesDataExtendedImpl)tsd;
+				if(tse.type instanceof GaRoDataTypeI) {
+					if(tse.getIds().size() > 1) {
+						String gwId = tse.getIds().get(0);
+						String prop = System.getProperty("org.ogema.evaluationofflinecontrol.scheduleviewer.expert.sensorsToFilterOut."+gwId);
+						if(prop != null) {
+							List<String> sensorsToFilterOut = Arrays.asList(prop.split(","));
+							String shortId = tse.getProperty("deviceName");
+							if(shortId != null)
+								if(sensorsToFilterOut.contains(shortId)) continue;
+						}
+					}
+					String location = tsd.label(null);
+					if(tsId == null) tsId = location;
+					dataType = (GaRoDataTypeI)tse.type;
+					String inputLabel = dataType.label(null).replace("Measurement", "");
+					if((tse.getIds().size() > 1) && tse.getIds().get(1).equals(GaRoMultiEvalDataProvider.BUILDING_OVERALL_ROOM_ID))
+						shortName = StringListFormatUtils.getStringFromList(null, tse.getIds().get(0), getDeviceShortId(location), inputLabel);
+					else
+						shortName = StringListFormatUtils.getStringFromList(tse.getIds(), getDeviceShortId(location), inputLabel);
+					longName = StringListFormatUtils.getStringFromList(tse.getIds(), tsd.label(null), inputLabel);
+				} else {
+					if(tse.type instanceof Class)
+						type = (Class<? extends Resource>)tse.type;
+					if(tse.getIds() == null) {
+						shortName = tsd.label(null);
+						longName = StringListFormatUtils.getStringFromList(null, "NoGw", tsd.label(null));
+					} else {
+						shortName = StringListFormatUtils.getStringFromList(tse.getIds());
+						longName = StringListFormatUtils.getStringFromList(tse.getIds(), tsd.label(null));
+					}
+				}
+			} else {
+				shortName = tsd.label(null);
+				longName = tsd.description(null);
+			}
+			if(tsId == null) {
+				shortNames.put(timeSeries, shortName);
+				longNames.put(timeSeries, longName);
+				if(dataType != null) types.put(timeSeries, dataType.representingResourceType());
+				else if(type != null) types.put(timeSeries, type);
+			} else {
+				shortNamesRD.put(tsId, shortName);
+				longNamesRD.put(tsId, longName);						
+				if(dataType != null) typesRD.put(tsId, dataType.representingResourceType());
+				else if(type != null) typesRD.put(tsId, type);
+			}
+			if(timeSeries != null) result.timeSeries.add(timeSeries);
+		}
+		List<TimeSeriesFilterExtended> programsInner = new ArrayList<>();
+		programsInner.add(new DefaultTimeSeriesFilterExtended(filterName, shortNames, longNames,
+				shortNamesRD, longNamesRD, types, typesRD));
+		result.filters = programsInner;
+		return result;
+	}
 }
