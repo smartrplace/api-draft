b72d8ef92e61f61c17334fa60096d722dda5b546
David Nestle <david.nestle@iee.fraunhofer.de>
2020-04-25 17:31:55 +0200

    added mobile user authentication to servlet

--- src/util-public/smartrplace-util-public/src/main/java/org/smartrplace/util/frontend/servlet/UserServletTest.java
+++ src/util-public/smartrplace-util-public/src/main/java/org/smartrplace/util/frontend/servlet/UserServletTest.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.ogema.accesscontrol.RestAccess;
+
 /** Core class to provide testing variants for servlets based on widgets pages. This servlet
  * does only provide relevant information in the HTTP response when the property 
  * org.smartrplace.apps.heatcontrol.servlet.istestinstance is set true. This shall
@@ -15,6 +17,9 @@ import javax.servlet.http.HttpServletResponse;
 */
 public abstract class UserServletTest extends HttpServlet {
 	protected abstract UserServlet getUserServlet();
+	protected RestAccess getRESTAccess() {
+		return null;
+	};
 	
 	/** This is the default property check that may be used also by other servlets if no special property is needed*/
 	protected String getPropertyToCheck() {
@@ -30,7 +35,7 @@ public abstract class UserServletTest extends HttpServlet {
 
     @Override
     protected void doOptions(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
-    	if(!isTestInstance(resp)) return;
+    	if(!checkAccessAllowedAndSendError(req, resp)) return;
     	resp.setCharacterEncoding("UTF-8");
     	resp.setContentType("application/json");
      	resp.addHeader("Access-Control-Allow-Origin", "*"); //CORS header
@@ -44,7 +49,7 @@ public abstract class UserServletTest extends HttpServlet {
 	@Override
     protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
     	//if(!req.getRequestURI().endsWith(SERVLET_ADDRESS)) return;
-    	if(!isTestInstance(resp)) return;
+       	if(!checkAccessAllowedAndSendError(req, resp)) return;
     	resp.setCharacterEncoding("UTF-8");
     	resp.setContentType("application/json");
     	try{
@@ -66,7 +71,7 @@ public abstract class UserServletTest extends HttpServlet {
     
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    	if(!isTestInstance(resp)) return;
+       	if(!checkAccessAllowedAndSendError(req, resp)) return;
     	resp.setCharacterEncoding("UTF-8");
     	resp.setContentType("application/json");
     	getUserServlet().doPost(req, resp);
@@ -78,19 +83,19 @@ public abstract class UserServletTest extends HttpServlet {
     
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    	if(!isTestInstance(resp)) return;
+       	if(!checkAccessAllowedAndSendError(req, resp)) return;
     	super.doPut(req, resp);
     }
     
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    	if(!isTestInstance(resp)) return;
+       	if(!checkAccessAllowedAndSendError(req, resp)) return;
     	super.doDelete(req, resp);
     }
     
     @Override
     protected void doHead(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    	if(!isTestInstance(resp)) return;
+       	if(!checkAccessAllowedAndSendError(req, resp)) return;
     	super.doHead(req, resp);
     }
     
@@ -106,14 +111,32 @@ public abstract class UserServletTest extends HttpServlet {
     	super.doTrace(arg0, arg1);
     }*/
     
-    public boolean isTestInstance(final HttpServletResponse resp) {
-    	if(!(Boolean.getBoolean(getPropertyToCheck())||Boolean.getBoolean("org.smartrplace.util.frontend.servlet.istestinstance"))) {
-    		try {
-				resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "This servlet can only be used on testinstances! ");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+    public boolean checkAccessAllowedAndSendError(final HttpServletRequest req, final HttpServletResponse resp) {
+    	boolean isTest = isTestInstance(resp);
+    	if(isTest)
+    		return true;
+    	RestAccess restAcc = getRESTAccess();
+    	if(restAcc == null) {
+    		sendError(resp);
+    		return false;
     	}
-    	return true;
+    	try {
+			return(restAcc.authenticate(req, resp) != null);
+		} catch (ServletException | IOException e) {
+			e.printStackTrace();
+			sendError(resp);
+			return false;
+		}
+    }
+    
+    public boolean isTestInstance(final HttpServletResponse resp) {
+    	return (Boolean.getBoolean(getPropertyToCheck())||Boolean.getBoolean("org.smartrplace.util.frontend.servlet.istestinstance"));
+    }
+    public void sendError(final HttpServletResponse resp) {
+		try {
+			resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "This servlet can only be used on testinstances! ");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 }
