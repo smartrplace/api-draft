ff9f3f8e7420506c5bcc676ef998feaee0090ff2
David Nestle <david.nestle@iee.fraunhofer.de>
2020-06-23 22:03:03 +0200

    added UserStatus

--- src/util-public/smartrplace-util-public/src/main/java/org/ogema/accessadmin/api/UserPermissionService.java
+++ src/util-public/smartrplace-util-public/src/main/java/org/ogema/accessadmin/api/UserPermissionService.java
@@ -6,6 +6,10 @@ import org.ogema.model.locations.Room;
 /** This service shall be offered to applications for checking whether relevant permissions are available
  * TODO: Move this to smartrplace-util or ogema-gui-api*/
 public interface UserPermissionService {
+	/** Rooms marked as priority are the primary rooms displayed to the user. Other rooms may be viewed, but
+	 * are not shown by default. Implies USER_ROOM_PERM*/
+	public static final String USER_PRIORITY_PERM = "UserRoomPiority";	
+	
 	/** General access permission to the room. If this is granted then checking for read or write
 	 * shall return true for both. If read/write are set separately then the integer of the most
 	 * specific permission set shall be returned. */
@@ -29,30 +33,46 @@ public interface UserPermissionService {
 	/** Permission to set back the setting for a {@link BuildingPropertyUnit} to the last
 	 * backup marked as safe.
 	 */
-	public static final String APP_RESTORE_SAFE_PERM = "AppRestoreSafePermission";
+	public static final String RESTORE_SAFE_PERM = "RestoreSafeStatePermission";
 
 	/** Permission to install an application or an application type in a certain
 	 * {@link BuildingPropertyUnit}
 	 */
-	public static final String APP_INSTALL_PERM = "AppInstallPermission";
+	public static final String UPDATE_INSTALL_PERM = "UpdateInstallPermission";
 	
 	/** Permission to generate backups for a BuildingPropertyUnit, to mark a backup as stable
 	 *  and to restore them*/
-	public static final String APP_RESTORE_PERM = "AppRestorePermission";
+	public static final String UPDATE_RATING_PERM = "UpdateRatingPermission";
 	
 	/** Permission to upload new apps with a certain permission level or updates for a certain app
 	 * into the Appstore.  Typically the main restrictions should now be imposed for the installation
 	 * of apps - upload for installation on test servers can be done even for critical apps quit easily.
 	 * For future public appstores this should be different, of course.
 	 */
-	public static final String APP_UPLOAD_PERM = "AppUploadPermission";
+	public static final String UPDATE_DEFINITION_PERM = "UpdateDefinitionPermission";
 
 	/** Permissions for which the rooms per user shall be chosen via Multiselect*/
-	public static final String[] ROOMPERMISSONS = {USER_ROOM_PERM, USER_READ_PERM, USER_WRITE_PERM,
+	public static final String[] ROOMPERMISSONS = {USER_PRIORITY_PERM, USER_ROOM_PERM,
+			USER_READ_HISTORICALDATA_PERM, USER_ADMIN_PERM };
+	/** The extended version is currently not used*/
+	public static final String[] ROOMPERMISSONS_EXTENDED = {USER_PRIORITY_PERM, USER_ROOM_PERM, USER_READ_PERM, USER_WRITE_PERM,
 			USER_CREATE_DELETE_PERM, USER_READ_HISTORICALDATA_PERM, USER_ADMIN_PERM };
 	
-	/** Permissions for which the building property units rooms per user shall be chosen via Multiselect*/
-	public static final String[] PROPUNITPERMISSIONS = {APP_RESTORE_SAFE_PERM, APP_INSTALL_PERM, APP_RESTORE_PERM};
+	/** User System Permissions. Some of them may be defined per Building Unit in the future.*/
+	public static final String[] PROPUNITPERMISSIONS = {RESTORE_SAFE_PERM, UPDATE_INSTALL_PERM, UPDATE_RATING_PERM,
+			UPDATE_DEFINITION_PERM};
+	
+	public static final String MONITORING = "Monitoring";
+	public static final String ROOM_STATUS_CONTROL = "Room Status Control";
+	public static final String INSTALLATION_SETUP = "Installation&Setup";
+	public static final String ALARMING = "Alarming";
+	public static final String USER_MANAGEMENT = "User Management";
+	public static final String GROUP_AND_PERMISSION_MANAGEMENT = "Permissions and Groups";
+	public static final String APPSTORE = "Appstore";
+	public static final String BACNET = "BACnet Admin";
+	
+	public static final String[] APP_ACCESS_PERMISSIONS = {MONITORING, ROOM_STATUS_CONTROL, INSTALLATION_SETUP, ALARMING,
+			GROUP_AND_PERMISSION_MANAGEMENT, APPSTORE, BACNET};
 	
 	/** Permissions for which the building property units rooms per user and per app or app permission type
 	 * shall be chosen via Multiselect. So the table will most likely only show one app or app permission type
@@ -64,7 +84,7 @@ public interface UserPermissionService {
 	/** Permission that are given per app permission type per user. So a Multiselect to choose the apps
 	 * instead of choosing the rooms could be used here. It could also be integrated
 	 * with PROPUNIT_PER_APP_PERMISSIONS and show as just a BooleanCheckBox there.*/
-	public static final String[] USER_APP_PERMISSIONS = {APP_UPLOAD_PERM};
+	public static final String[] USER_APP_PERMISSIONS = {UPDATE_DEFINITION_PERM};
 	
 	/** True if at least a read permission for the room is granted*/
 	boolean hasUserPermissionForRoom(String userName, Room room);
@@ -76,9 +96,16 @@ public interface UserPermissionService {
 	int getUserPermissionForRoom(String userName, Room room, String permissionType);
 
 	/** Returns the permission value that may be greater than one for a permission granted*/
-	int getUserPermissionForUnitApps(String userName, String unitName, String appName, String permissionType);
+	int getUserSystemPermission(String userName, String permissionType);
 
+	/** Permission types allowed here are from APP_ACCESS_PERMISSIONS*/
+	int getUserStatusAppPermission(UserStatus userStatus, String permissionType);
+	
 	/** Returns the permission value that may be greater than one for a permission granted*/
+	//int getUserPermissionForUnitApps(String userName, String unitName, String appName, String permissionType);
+
+	/** Returns the permission value that may be greater than one for a permission granted
+	 * Note: Management of these permissions not implemented yet*/
 	int getUserPermissionForApp(String userName, String appName, String permissionType);
 
 	/** True if at least a read permission for the data point is granted*/
@@ -92,4 +119,6 @@ public interface UserPermissionService {
 	//int[] getPermissionValuesForDatapoint(String userName, DatapointDesc datapoint);
 	
 	//TODO: Add methods to check directly for write, read and write
+	
+	//default 
 }
---
+++ src/util-public/smartrplace-util-public/src/main/java/org/ogema/accessadmin/api/UserStatus.java
@@ -0,0 +1,47 @@
+package org.ogema.accessadmin.api;
+
+import de.iwes.widgets.api.OgemaGuiService;
+import de.iwes.widgets.api.widgets.localisation.OgemaLocale;
+
+/** Basic user types defined in the system*/
+public enum UserStatus {
+	/** A defect user does not have the app permissions defined in BASE_APPS. This user cannot
+	 * perform the standard logout process, so such a user should not exist on the system.
+	 */
+	DISABLED,
+	/** A raw user does not have all permissions in USER_APPS. Such a user cannot acsess the
+	 * basic SmartrRoomControl user apps*/
+	//RAW,
+	/** A standard user can access the basic SmartrRoomControl user apps, but not all admin apps*/ 
+	USER_STD,
+	/** A secretary user can access at least all apps defined in USER_STD, but usually has additional
+	 * app permissions*/
+	SECRETARY,
+	/** An admin user can access at least all apps defined in ADMIN_APPS*/
+	ADMIN,
+	/** Super admin has access to framework administration, full message settings etc.*/
+	SUPERADMIN,;
+
+	/** Only access to standard room control interface*/
+	//GUEST,
+	/** Like ADMIN, also access to simulation GUI*/
+	//TESTER
+	
+	public static String getLabel(UserStatus obj, OgemaLocale locale) {
+		switch(obj) {
+		case DISABLED:
+			return "Disabled";
+		case USER_STD:
+			return "User Standard";
+		case SECRETARY:
+			return "Secretary";
+		case ADMIN:
+			return "Administrator";
+		case SUPERADMIN:
+			return "Master Administrator";
+		default:
+			throw new IllegalStateException("Unknown type:"+obj);
+		}
+	}
+
+}
