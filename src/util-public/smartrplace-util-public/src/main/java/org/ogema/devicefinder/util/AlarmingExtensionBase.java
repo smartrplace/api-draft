6b281f6b9df20d0c8abdb925b26578fb4d8a66e2
David Nestle <david.nestle@iee.fraunhofer.de>
2020-08-19 14:48:15 +0200

    update alarming

---
+++ src/util-public/smartrplace-util-public/src/main/java/org/ogema/devicefinder/util/AlarmingExtensionBase.java
@@ -0,0 +1,63 @@
+package org.ogema.devicefinder.util;
+
+import org.ogema.core.model.simple.BooleanResource;
+import org.ogema.core.model.simple.FloatResource;
+import org.ogema.core.model.simple.SingleValueResource;
+import org.ogema.devicefinder.api.AlarmingExtension;
+import org.ogema.devicefinder.api.AlarmingExtensionListener;
+import org.ogema.model.extended.alarming.AlarmConfiguration;
+import org.ogema.tools.resourcemanipulator.timer.CountDownDelayedExecutionTimer;
+
+public abstract class AlarmingExtensionBase implements AlarmingExtension {
+
+	public static class ValueListenerDataBase {
+		public ValueListenerDataBase(FloatResource res) {
+			this.res = res;
+			this.bres = null;
+		}
+		public ValueListenerDataBase(BooleanResource bres) {
+			this.res = null;
+			this.bres = bres;
+		}
+		//public AlarmValueListenerI listener;
+		public final FloatResource res;
+		public final BooleanResource bres;
+		public CountDownDelayedExecutionTimer timer = null;
+		public CountDownDelayedExecutionTimer alarmReleaseTimer = null;
+		public long nextTimeAlarmAllowed = -1;
+		public boolean isAlarmActive = false;
+		public boolean isNoValueAlarmActive = false;
+		
+		//supervision for last data received
+		public long lastTimeOfNewData = -1;
+		public long maxIntervalBetweenNewValues;
+	}
+
+	protected class AlarmExtListenerBase implements AlarmingExtensionListener {
+		public final ValueListenerDataBase valueListener;
+		
+		public AlarmExtListenerBase(SingleValueResource res, AlarmConfiguration ac) {
+			if(ac.sensorVal() instanceof FloatResource)
+				valueListener = new ValueListenerDataBase((FloatResource) ac.sensorVal());
+			else if(ac.sensorVal() instanceof BooleanResource)
+				valueListener = new ValueListenerDataBase((BooleanResource) ac.sensorVal());
+			else
+				throw new IllegalArgumentException("Only Float- and BooleanResource supported!");
+		}
+		
+		@Override
+		public <T extends SingleValueResource> AlarmResult resourceChanged(T resource, float value, long now) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+	
+	@Override
+	public AlarmingExtensionListener getListener(SingleValueResource res, AlarmConfiguration ac) {
+		final Long maxTime = (long) Math.max(ac.maxIntervalBetweenNewValues().getValue(),
+				ac.maxViolationTimeWithoutAlarm().getValue())*60000;
+		return new AlarmExtListenerBase(res, ac);
+	}
+
+}
