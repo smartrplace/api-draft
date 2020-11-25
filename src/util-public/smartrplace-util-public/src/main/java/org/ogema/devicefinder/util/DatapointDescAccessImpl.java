8f59258aa77d5dd7ad8cca82895135171f1f35bb
David Nestle <david.nestle@iee.fraunhofer.de>
2020-11-25 22:27:20 +0100

    clean up some datapoint printouts

--- src/util-public/smartrplace-util-public/src/main/java/org/ogema/devicefinder/util/DatapointDescAccessImpl.java
+++ src/util-public/smartrplace-util-public/src/main/java/org/ogema/devicefinder/util/DatapointDescAccessImpl.java
@@ -47,17 +47,17 @@ public class DatapointDescAccessImpl extends DatapointDescImpl implements Datapo
 	@Override
 	public boolean setSubRoomLocation(OgemaLocale locale, Object context, String value) {
 		if(subRoomLocation == null) {
-if(id().contains("subPhaseConnections"))
-System.out.println("Val:"+value+"  Id:"+id()+"  Instance:"+instanceCount);
+//if(id().contains("subPhaseConnections"))
+//System.out.println("Val:"+value+"  Id:"+id()+"  Instance:"+instanceCount);
 			subRoomLocation = value;
 			return true;
 		}
 			
 		synchronized(subRoomLocation) {
-if(id().contains("subPhaseConnections"))
-System.out.println("Val:"+value+"  Id:"+id()+"  Instance:"+instanceCount);
-if(id().contains("subPhaseConnections") && (subRoomLocation != null) &&  (subRoomLocation.length() > value.length()))
-System.out.println("Existing:"+subRoomLocation+"Val:"+value);
+//if(id().contains("subPhaseConnections"))
+//System.out.println("Val:"+value+"  Id:"+id()+"  Instance:"+instanceCount);
+//if(id().contains("subPhaseConnections") && (subRoomLocation != null) &&  (subRoomLocation.length() > value.length()))
+//System.out.println("Existing:"+subRoomLocation+"Val:"+value);
 
 			subRoomLocation = value;
 		}
