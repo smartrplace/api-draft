da63247bf01dbc2573d4d3d6042f3c3c9a3b6fd6
David Nestle <david.nestle@iee.fraunhofer.de>
2020-11-27 22:01:09 +0100

    update DriverPropertySuccessHandler

--- src/util-public/smartrplace-util-public/src/main/java/org/ogema/devicefinder/api/DriverPropertySuccessHandler.java
+++ src/util-public/smartrplace-util-public/src/main/java/org/ogema/devicefinder/api/DriverPropertySuccessHandler.java
@@ -5,10 +5,10 @@ import org.ogema.core.model.Resource;
 public interface DriverPropertySuccessHandler<T extends Resource> {
 	/** Notification that a requested read or write operation is finished
 	 * 
-	 * @param dataPointResource
+	 * @param anchorResource anchor resource holding the actual propertyNames and Values StringArrayResources
 	 * @param propertyId may be null e.g. if all propertyIds were requested or processed
 	 * @param success if false the operation was not successful
 	 * @param message further information on errors or success status, may be null
 	 */
-	void operationFinished(T dataPointResource, String propertyId, boolean success, String message);
+	void operationFinished(T anchorResource, String propertyId, boolean success, String message);
 }
