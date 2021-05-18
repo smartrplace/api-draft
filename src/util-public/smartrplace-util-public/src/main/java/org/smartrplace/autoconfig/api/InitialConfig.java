7de42b2581d1e080649df1e18652e26c180be781
David Nestle <david.nestle@iee.fraunhofer.de>
2021-05-18 22:14:38 +0200

    add clean up to InitialConfig

--- src/util-public/smartrplace-util-public/src/main/java/org/smartrplace/autoconfig/api/InitialConfig.java
+++ src/util-public/smartrplace-util-public/src/main/java/org/smartrplace/autoconfig/api/InitialConfig.java
@@ -14,13 +14,27 @@ public class InitialConfig {
 	}
 	
 	public static void addString(String initID, StringResource res) {
+		addString(initID, res, null);
+	}
+	public static void addString(String initID, StringResource res, String deviceId) {
 		if(!res.exists()) {
 			ValueResourceHelper.setCreate(res, initID);
 		} else {
 			String exist = res.getValue();
 			if(exist.contains(initID+","))
 				return;
-			res.setValue(exist+initID+",");
+			String newExist = "";
+			if(deviceId != null) {
+				//remove existing entries for deviceId
+				String[] entries = exist.split(",");
+				for(String e: entries) {
+					if(e.startsWith(deviceId))
+						continue;
+					newExist += e+",";
+				}
+			} else
+				newExist = exist;
+			res.setValue(newExist+initID+",");
 		}
 	}
 
