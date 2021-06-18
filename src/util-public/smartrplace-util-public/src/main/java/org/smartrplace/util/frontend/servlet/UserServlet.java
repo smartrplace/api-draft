1153cb94798c2934e1848fdaae7d31806b47c7df
David Nestle <david.nestle@iee.fraunhofer.de>
2021-06-18 18:50:59 +0200

    fix UserServlet

--- src/util-public/smartrplace-util-public/src/main/java/org/smartrplace/util/frontend/servlet/UserServlet.java
+++ src/util-public/smartrplace-util-public/src/main/java/org/smartrplace/util/frontend/servlet/UserServlet.java
@@ -581,7 +581,7 @@ public class UserServlet extends HttpServlet {
 				startIdx = 1;
 			} else
 				startIdx = 0;
-			for(idx=startIdx; idx<subURL.length-2; idx+=2) {
+			for(idx=startIdx; idx<subURL.length-1; idx+=2) {
 				String paramName = subURL[idx];
 				String param = subURL[idx+1];
 				addParameter(paramName, param, paramMap);
