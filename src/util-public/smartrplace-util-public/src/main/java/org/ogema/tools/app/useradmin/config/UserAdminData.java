271ff8a05779c5fd1ba94619906571b3aa7e2dfb
David Nestle <david.nestle@iee.fraunhofer.de>
2021-10-23 13:58:53 +0200

    add user invite management to UserAdminData model

--- src/util-public/smartrplace-util-public/src/main/java/org/ogema/tools/app/useradmin/config/UserAdminData.java
+++ src/util-public/smartrplace-util-public/src/main/java/org/ogema/tools/app/useradmin/config/UserAdminData.java
@@ -1,6 +1,7 @@
 package org.ogema.tools.app.useradmin.config;
 
 import org.ogema.core.model.ResourceList;
+import org.ogema.core.model.simple.IntegerResource;
 import org.ogema.model.prototypes.Data;
 import org.ogema.model.user.NaturalPerson;
 
@@ -8,4 +9,11 @@ public interface UserAdminData extends Data {
 	ResourceList<NaturalPerson> userData();
 	
 	ResourceList<RESTUserData> restUserData();
+	
+	/** If zero, negative or not existing then no invite messages are sent when a new user is created. This is the
+	 * default for system setup. Only after finishing setup then this shall be activated and missing invites
+	 * are sent out automatically.<br>
+	 * A negative value indicates a temporary switch off. Invites will not be sent out automatically when enabled again.
+	 */
+	IntegerResource enableInviteMessagesForUserCreation();
 }
