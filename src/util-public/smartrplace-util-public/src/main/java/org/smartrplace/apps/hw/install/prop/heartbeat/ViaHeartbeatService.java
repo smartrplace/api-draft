38f3c693381b9b4cbb941ceeb006f6519cbae015
David Nestle <david.nestle@iee.fraunhofer.de>
2022-01-03 18:43:54 +0100

    minor update ViaHeartbeatService

--- src/util-public/smartrplace-util-public/src/main/java/org/smartrplace/apps/hw/install/prop/heartbeat/ViaHeartbeatService.java
+++ src/util-public/smartrplace-util-public/src/main/java/org/smartrplace/apps/hw/install/prop/heartbeat/ViaHeartbeatService.java
@@ -1,5 +1,7 @@
 package org.smartrplace.apps.hw.install.prop.heartbeat;
 
+import org.ogema.core.model.Resource;
+
 public interface ViaHeartbeatService {
 	/** A connection must be registered with the same id both on client and server side. Usually this is done
 	 * by an application that runs both client and server side and that uses the same method call on both sides.
@@ -21,4 +23,23 @@ public interface ViaHeartbeatService {
 	 * server-side-only registration.
 	 * TODO: This may be available in the future*/
 	//HeartbeatConnection registerConnection(String id);
+	
+	public enum ChangeType {
+		/**IF the resource is created and activated this should be notified as a single change*/
+		CREATED_INACTIVE,
+		CREATED_ACTIVE,
+		CREATED_REFERENCE,
+		//ACTIVATED,
+		DELETED,
+		//DEACTIVATED
+	}
+	/** If a resource and sub resources are changed, then only the most toplevel resource
+	 * shall be notified here. If a new reference is set then no changes in the resource referenced
+	 * are checked. Before a reference is set the resource referenced should be provided in a
+	 * {@link HeartbeatConnectionRegistration} so that the right reference can be found.
+	 * @param res
+	 * @param changeType can usually be set null as this is determined automatically
+	 * @return The return type identified
+	 */
+	ChangeType resourceStructureChangeNotification(Resource res, ChangeType changeType);
 }
