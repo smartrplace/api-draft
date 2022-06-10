30658f1c0f47ee623636c81d1b78d22756a8748f
David Nestle <david.nestle@iee.fraunhofer.de>
2022-06-10 18:51:11 +0200

    add BACnet Admin to Superadmin App-Admin

--- src/util-public/smartrplace-util-public/src/main/java/org/ogema/accessadmin/api/HeatCoolData.java
+++ src/util-public/smartrplace-util-public/src/main/java/org/ogema/accessadmin/api/HeatCoolData.java
@@ -98,32 +98,36 @@ public class HeatCoolData extends SettingsBaseData {
 	}
 	
 	public static int STARTEND_WORKINGDAY_IDX = 7;
-	public static int STARTEND_TIMES_SIZE = 8;
+	public static int STARTEND_WEEKEND_IDX = 8;
+	public static int STARTEND_TIMES_SIZE = 9;
 	public Map<Integer, List<Long>> startEndTimes;
 	
-	public List<Long> startEndTimesMonday() {
+	public List<Long> getStartEndTimesMonday() {
 		return startEndTimes.get(0);
 	}
-	public List<Long> startEndTimesTuesday() {
+	public List<Long> getStartEndTimesTuesday() {
 		return startEndTimes.get(1);
 	}
-	public List<Long> startEndTimesWednesday() {
+	public List<Long> getStartEndTimesWednesday() {
 		return startEndTimes.get(2);
 	}
-	public List<Long> startEndTimesThursday() {
+	public List<Long> getStartEndTimesThursday() {
 		return startEndTimes.get(3);
 	}
-	public List<Long> startEndTimesFriday() {
+	public List<Long> getStartEndTimesFriday() {
 		return startEndTimes.get(4);
 	}
-	public List<Long> startEndTimesSaturday() {
+	public List<Long> getStartEndTimesSaturday() {
 		return startEndTimes.get(5);
 	}
-	public List<Long> startEndTimesSunday() {
+	public List<Long> getStartEndTimesSunday() {
 		return startEndTimes.get(6);
 	}
-	public List<Long> startEndTimesWorkingday() {
+	public List<Long> getStartEndTimesWorkingday() {
 		return startEndTimes.get(STARTEND_WORKINGDAY_IDX);
+	}
+	public List<Long> getStartEndTimesWeekend() {
+		return startEndTimes.get(STARTEND_WEEKEND_IDX);
 	}
 	
 	public long getStartTime() {
@@ -145,5 +149,33 @@ public class HeatCoolData extends SettingsBaseData {
 	}
 	public void setEndTime(long endTime) {
 		endTimeFromRoomData = endTime;
+	}
+	
+	public void setStartEndTimesMonday(List<Long> tm) {
+		startEndTimes.put(0, tm);
+	}
+	public void setStartEndTimesTuesday(List<Long> tm) {
+		startEndTimes.put(1, tm);
+	}
+	public void setStartEndTimesWednesday(List<Long> tm) {
+		startEndTimes.put(2, tm);
+	}
+	public void setStartEndTimesThursday(List<Long> tm) {
+		startEndTimes.put(3, tm);
+	}
+	public void setStartEndTimesFriday(List<Long> tm) {
+		startEndTimes.put(4, tm);
+	}
+	public void setStartEndTimesSaturday(List<Long> tm) {
+		startEndTimes.put(5, tm);
+	}
+	public void setStartEndTimesSunday(List<Long> tm) {
+		startEndTimes.put(6, tm);
+	}
+	public void setStartEndTimesWorkingday(List<Long> tm) {
+		startEndTimes.put(STARTEND_WORKINGDAY_IDX, tm);
+	}
+	public void setStartEndTimesWeekend(List<Long> tm) {
+		startEndTimes.put(STARTEND_WEEKEND_IDX, tm);
 	}
 }
