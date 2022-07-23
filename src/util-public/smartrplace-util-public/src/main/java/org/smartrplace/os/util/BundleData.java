032a52f8aee2922c2903fd1d5e471b5cedc62fe6
David Nestle <david.nestle@iee.fraunhofer.de>
2022-07-23 19:01:18 +0200

    update BundleData

--- src/util-public/smartrplace-util-public/src/main/java/org/smartrplace/os/util/BundleData.java
+++ src/util-public/smartrplace-util-public/src/main/java/org/smartrplace/os/util/BundleData.java
@@ -1,22 +1,28 @@
 package org.smartrplace.os.util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.ogema.core.administration.AdminApplication;
 import org.osgi.framework.Bundle;
+
+import de.iwes.widgets.api.widgets.localisation.OgemaLocale;
 
 public class BundleData {
 	public BundleData(Bundle bd) {
 		this.bundle = bd;
-		this.appTitle = null;
+		//this.appTitle = null;
 	}
 	public BundleData(Bundle bd, String appTitle) {
 		this.bundle = bd;
-		this.appTitle = appTitle;
+		this.appTitle.put(OgemaLocale.ENGLISH, appTitle);
 	}
 
 	public Bundle bundle;
 	public int position;
 	
-	public String appTitle;
+	public Map<OgemaLocale, String> appTitle = new HashMap<>();
+	public Map<OgemaLocale, String> description = new HashMap<>();
 	
 	public AdminApplication entry;
 }
