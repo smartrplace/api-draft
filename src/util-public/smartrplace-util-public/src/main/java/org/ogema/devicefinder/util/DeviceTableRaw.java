9901a0d43f4dd8375078eb8faeda9c583a64d8d5
David Nestle <david.nestle@iee.fraunhofer.de>
2022-11-08 15:57:07 +0100

    update isWallThermostat

--- src/util-public/smartrplace-util-public/src/main/java/org/ogema/devicefinder/util/DeviceTableRaw.java
+++ src/util-public/smartrplace-util-public/src/main/java/org/ogema/devicefinder/util/DeviceTableRaw.java
@@ -722,6 +722,8 @@ public abstract class DeviceTableRaw<T, R extends Resource> extends ObjectGUITab
 	public static boolean isWallThermostat(String resourceLocation) {
 		if(resourceLocation.contains("HM_HmIP_WTH_"))
 			return true;
+		if(resourceLocation.contains("HM_HmIP_BWTH_"))
+			return true;
 		if(resourceLocation.contains("HM_HM_TC_IT_WM_W_EU"))
 			return true;
 		return false;
