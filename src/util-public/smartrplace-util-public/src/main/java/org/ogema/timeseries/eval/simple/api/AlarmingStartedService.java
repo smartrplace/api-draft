f4aaf6b7120fce5f10e22feabfd182eb8d3ff1f6
David Nestle <david.nestle@iee.fraunhofer.de>
2023-07-12 18:23:25 +0200

    update AlarmingStartedService

--- src/util-public/smartrplace-util-public/src/main/java/org/ogema/timeseries/eval/simple/api/AlarmingStartedService.java
+++ src/util-public/smartrplace-util-public/src/main/java/org/ogema/timeseries/eval/simple/api/AlarmingStartedService.java
@@ -1,7 +1,13 @@
 package org.ogema.timeseries.eval.simple.api;
 
+import org.ogema.core.model.ValueResource;
+import org.ogema.devicefinder.util.AlarmingExtensionBase.ValueListenerDataBase;
+
 /** Service indicating that alarming has fully started after system restart*/
 public interface AlarmingStartedService {
 	/** As soon as the service is available this should always be true*/
 	boolean isAlarmingStarted();
+	
+	/** Get alarming internal parameter data */
+	ValueListenerDataBase getValueListenerData(ValueResource res);
 }
