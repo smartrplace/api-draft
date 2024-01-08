fbdaa8412ec2bad9646ad31316aa81797916ddad
dnestle <dnestle@DESKTOP-895GNE8>
2024-01-08 17:32:30 +0100

    add deleteTimedJobService

--- src/util-public/smartrplace-util-public/src/main/java/org/ogema/devicefinder/api/TimedJobMgmtService.java
+++ src/util-public/smartrplace-util-public/src/main/java/org/ogema/devicefinder/api/TimedJobMgmtService.java
@@ -2,6 +2,8 @@ package org.ogema.devicefinder.api;
 
 import java.util.Collection;
 
+import org.smartrplace.apps.eval.timedjob.TimedJobConfig;
+
 public interface TimedJobMgmtService {
 	/** Register provider. If the ID is already known, do nothing
 	 * @param prov
@@ -12,6 +14,9 @@ public interface TimedJobMgmtService {
 	/** Unregister provider. Usaully this should not be necessary
 	 * @return data for the service unregistered. Null if job is not registered or running (cannot be unregistered then)*/
 	TimedJobMemoryData unregisterTimedJobProvider(TimedJobProvider prov);
+	
+	/** Delete persistent data for timed job. You have to make sure the job is not registered anymore.*/
+	public TimedJobConfig deleteTimedJobProvider(String id);
 	
 	Collection<TimedJobMemoryData> getAllProviders();
 	
