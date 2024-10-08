b94c0972dfbdbf5e7358605c1a44e0acdd35f22f
dnestle <dnestle@DESKTOP-895GNE8>
2024-10-08 11:53:52 +0200

    update DeviceTableRaw

--- src/util-public/smartrplace-util-public/src/main/java/org/ogema/devicefinder/util/DeviceTableRaw.java
+++ src/util-public/smartrplace-util-public/src/main/java/org/ogema/devicefinder/util/DeviceTableRaw.java
@@ -965,6 +965,8 @@ public abstract class DeviceTableRaw<T, R extends Resource> extends ObjectGUITab
 			return true;
 		if(resourceLocation.contains("HM_HmIP_BWTH_"))
 			return true;
+		if(resourceLocation.contains("HM_HmIP_BWTH24_"))
+			return true;
 		if(resourceLocation.contains("HM_HM_TC_IT_WM_W_EU"))
 			return true;
 		return false;
