5da1fcdd7298a0cac7a5b24b98476e7dc043919a
dnestle <dnestle@172.31.128.2>
2025-09-17 17:41:37 +0200

    full support for long in ServletNumProvider

--- src/util-public/smartrplace-util-public/src/main/java/org/smartrplace/util/frontend/servlet/ServletNumProvider.java
+++ src/util-public/smartrplace-util-public/src/main/java/org/smartrplace/util/frontend/servlet/ServletNumProvider.java
@@ -2,12 +2,12 @@ package org.smartrplace.util.frontend.servlet;
 
 import org.ogema.core.channelmanager.measurements.BooleanValue;
 import org.ogema.core.channelmanager.measurements.FloatValue;
-import org.ogema.core.channelmanager.measurements.IntegerValue;
+import org.ogema.core.channelmanager.measurements.LongValue;
 import org.ogema.core.channelmanager.measurements.Value;
 
 public class ServletNumProvider extends ServletNumProviderBase {
 	protected final Float floatVal;
-	protected final Integer intVal;
+	protected final Long intVal;
 	protected final Boolean booleanVal;
 
 	public ServletNumProvider(float val) {
@@ -22,18 +22,18 @@ public class ServletNumProvider extends ServletNumProviderBase {
 	}
 	public ServletNumProvider(int val) {
 		floatVal = null;
-		intVal = val;
+		intVal = (long) val;
 		booleanVal = null;
 	}
 		
 	public ServletNumProvider(long val) {
-		if(val <= Integer.MAX_VALUE && val >= Integer.MIN_VALUE) {
-			intVal = (int) val;
+		//if(val <= Integer.MAX_VALUE && val >= Integer.MIN_VALUE) {
+			intVal = val;
 			floatVal = null;
-		} else {
+		/*} else {
 			floatVal = (float) val;
 			intVal = null;			
-		}
+		}*/
 		booleanVal = null;
 	}
 
@@ -41,7 +41,7 @@ public class ServletNumProvider extends ServletNumProviderBase {
 		if(floatVal != null)
 			return new FloatValue(floatVal);
 		else if(intVal != null)
-			return new IntegerValue(intVal);
+			return new LongValue(intVal);
 		else if(booleanVal != null)
 			return new BooleanValue(booleanVal);
 		throw new IllegalStateException("no value defined!");
