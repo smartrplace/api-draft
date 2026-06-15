1cfd188774ba0c2079cc5db5509eb30330e5ab93
unknown <david.nestle@smartrplace.de>
2026-06-15 18:14:18 +0200

    add wall temperature sensor support

--- src/util-public/smartrplace-util-public/src/main/java/org/ogema/devicefinder/util/DeviceTableRaw.java
+++ src/util-public/smartrplace-util-public/src/main/java/org/ogema/devicefinder/util/DeviceTableRaw.java
@@ -1134,6 +1134,17 @@ public abstract class DeviceTableRaw<T, R extends Resource> extends ObjectGUITab
 			return true;
 		return false;
 	}
+	public static boolean isWallThermostatReal(String resourceLocation) {
+		if(resourceLocation.contains("HM_HmIP_WTH_"))
+			return true;
+		if(resourceLocation.contains("HM_HmIP_BWTH_"))
+			return true;
+		if(resourceLocation.contains("HM_HmIP_BWTH24_"))
+			return true;
+		if(resourceLocation.contains("HM_HM_TC_IT_WM_W_EU"))
+			return true;
+		return false;
+	}
 	public static boolean isCLThermostat(String resourceLocation) {
 		if(resourceLocation.contains("HmIP_eTRV_CL_"))
 			return true;
