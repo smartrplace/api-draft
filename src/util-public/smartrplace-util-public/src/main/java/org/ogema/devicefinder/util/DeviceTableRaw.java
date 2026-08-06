aea5b07230a024c36fe19de5c55369aacea4fa7c
unknown <david.nestle@smartrplace.de>
2026-08-06 20:38:36 +0200

    make WT303 wall thermostat(2)

--- src/util-public/smartrplace-util-public/src/main/java/org/ogema/devicefinder/util/DeviceTableRaw.java
+++ src/util-public/smartrplace-util-public/src/main/java/org/ogema/devicefinder/util/DeviceTableRaw.java
@@ -1257,6 +1257,8 @@ public abstract class DeviceTableRaw<T, R extends Resource> extends ObjectGUITab
 			return true;
 		if(resourceLocation.contains("HM_HmIP_STH_"))
 			return true;
+		if(resourceLocation.contains("ms_wt303"))
+			return true;
 		return false;
 	}
 	public static boolean isWallThermostatReal(String resourceLocation) {
